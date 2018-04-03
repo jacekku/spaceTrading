e7d8c4b274977d92a38f02cf52eb8842ddb78824
jacekku <jacekku@github.com>
2018-04-03 21:50:48 +0200

    0.5 updated some translation functions and a background function

--- graphicFunctions.js
+++ graphicFunctions.js
@@ -15,3 +15,21 @@ function strokeEllipse(x,y,r1,r2,fill,rotation=0,startAngle=0,endAngle=Math.PI*2
     c.ellipse(x,y,r1,r2,rotation,startAngle,endAngle)
     c.stroke()
 }
+function background(fill){
+    c.fillStyle=fill
+    rect(0,0,WIDTH,HEIGHT)
+}
+function createCanvas(width,height){}
+
+function pushState(){
+    c.save()
+}
+function popState(){
+    c.restore()
+}
+function translate(x,y){
+    c.translate(x,y)
+}
+function rotate(radians){
+    c.rotate(radians)
+}
